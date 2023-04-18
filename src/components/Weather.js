159fc676aca808272ecb24997a068e04ede4e677
Karan Kumar <karankumar072508@gmail.com>
2023-04-19 00:26:01 +0530

    make some changes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,40 +1,52 @@
 import React from "react";
 import {useState, useEffect} from 'react';
+import '../styles/Weather.css';
 
 function Weather(){
 
     const apiKey = 'de26d68544e4fb8e272b852437ea6e3d';
 
-    const cityName = "Delhi";
+    const cityName = "las vegas";
 
     // Createing the hook that shows data when we fetch from api --- suitable Data
     const[mainTemp, setMainTemp] = useState('');
     const[description, setDescription] = useState('');
     const[iconId, setIconId] = useState('');
     const[main, setMain] = useState('');
+    const[place, setPlace] = useState('');
 
     
 
 
     //Here we create the useEffect hook which will run when the ComponentIsMount --(ComponentDidMount) for the first time on the page.
     useEffect(() => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
             .then((res) => res.json())
             .then((data) => {
                 console.log("api data >> ", data);
                 setMain(data.weather[0].main);
                 setDescription(data.weather[0].description);
-                setMainTemp(data.main.temp -272.15);
+                setMainTemp(data.main.temp);
                 setIconId(data.weather[0].icon);
+                setPlace(data.name);
             })
     }, [])
 
     return(
         <React.Fragment>
-            <h1>This is my Weather app</h1>
+            <h1> Weather App</h1>
+            
+            <input 
+                placeholder="enter the name of city/state/country"                 
+            />
+            <span>
+                <button>Search</button>
+            </span>
+            <h1>{place}</h1>
             <h2>Main Temprature : {mainTemp} Degree Celsius</h2>
             <h2>Temprature Descripiton : {description} </h2>
             <h2>Weather Parameter : {main}</h2>
+            <img src={"http://openweathermap.org/img/wn/" + iconId + "@2x.png"}/>
             {/* <h2>Main Temprature : {mainTemp}</h2> */}
         </React.Fragment>
     )
