473353538b652848c7b7d303a6b9b0bdf8daffc6
Karan Kumar <karankumar072508@gmail.com>
2023-05-04 23:18:34 +0530

    modifing utils file

--- src/apis/utiils.js
+++ src/apis/utiils.js
@@ -1,1 +1,14 @@
-export const apiKey = 'de26d68544e4fb8e272b852437ea6e3d';
+const API_KEY = 'de26d68544e4fb8e272b852437ea6e3d';
+
+export const getFormattedData = async(city) => {
+
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    const data = await fetch(URL)
+        .then((res) => res.json())
+        .then((data) => data );
+
+
+    console.log(data);
+
+}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,7 +7,7 @@ function Weather(){
 
 
 
-    const cityName = "Bangalore";
+    const [cityName, setCityName] = useState("");
 
     // Createing the hook that shows data when we fetch from api --- suitable Data
     const[mainTemp, setMainTemp] = useState('');
@@ -20,35 +20,47 @@ function Weather(){
 
 
     //Here we create the useEffect hook which will run when the ComponentIsMount --(ComponentDidMount) for the first time on the page.
-    useEffect(() => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log("api data >> ", data);
-                setMain(data.weather[0].main);
-                setDescription(data.weather[0].description);
-                setMainTemp(data.main.temp);
-                setIconId(data.weather[0].icon);
-                setPlace(data.name);
-            })
-    }, [])
+    // useEffect(() => {
+    //     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             console.log("api data >> ", data);
+    //             setMain(data.weather[0].main);
+    //             setDescription(data.weather[0].description);
+    //             setMainTemp(data.main.temp);
+    //             setIconId(data.weather[0].icon);
+    //             setPlace(data.name);
+    //         })
+    // }, [])
+
+
+    // handle city Name
+    const handleCityName = (e) => {
+        e.preventDefault();
+
+        setCityName(e.target.value);
+        console.log(cityName);
+    }
 
     return(
         <React.Fragment>
             <div className="container">
-                <h1> Weather App</h1>
+                <h1> Weather In</h1>
                 
                 <input 
-                    placeholder="search for a city...."                 
+                    className="searchInput"
+                    placeholder="search for a city...." 
+                    value={cityName}
+                    onChange = {handleCityName}                
                 />
                 {/* <span>
                     <button>Search</button>
                 </span> */}
                 <h1>{place}</h1>
-                <h2>Main Temprature : {mainTemp} Degree Celsius</h2>
+                <h2>Temp. : {mainTemp}<sup className="sup">.</sup> C</h2>
+                <img className="weather-image" src={"http://openweathermap.org/img/wn/" + iconId + "@2x.png"} alt="weather-logo"/>
                 <h2>Temprature Descripiton : {description} </h2>
                 <h2>Weather Parameter : {main}</h2>
-                <img src={"http://openweathermap.org/img/wn/" + iconId + "@2x.png"}/>
                 {/* <h2>Main Temprature : {mainTemp}</h2> */}
             </div>
         </React.Fragment>
